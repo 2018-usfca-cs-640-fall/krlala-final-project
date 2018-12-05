9735e9d4af052764ada4d7d6ac6913dfdd7465c2
Your Name <you@example.com>
2018-12-05 07:48:39 -0800

    Adds comments and fixes shellcheck error

--- scripts/download_rawdata.sh
+++ scripts/download_rawdata.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+# This script downloads raw data(fastq files) into the raw_data folder.
+# My name is karan lala, and email- krlala@usfca.edu
+echo "Downloading data"
 for SRA_number in $(cut -f 8 data/metadata/SraRunTablel.txt | tail -n +2)
+# for loop used for downloading file from each SRA number.
 do
-    SRATOOLKIT/bin/fastq-dump.exe -v $SRA_number -O data/raw_data
+    SRATOOLKIT/bin/fastq-dump.exe -v "$SRA_number" -O data/raw_data
 done
